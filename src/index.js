be1dee8173ef03afcbb6d360ec4d3a1dfbf4c8ef
vae <xiaochangvv@gmail.com>
2020-04-23 18:37:15 +0800

    react props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Welcome from './welcome';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function formatName(user) {
+  return user.firstName + '' + user.lastName;
+}
+
+const user = {
+  firstName: 'vae',
+  lastName: 'chang'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+  <div>
+     {element}
+     {/* //所有 React 组件都必须像纯函数一样保护它们的 props 不被更改 */}
+     <Welcome name="vae"/>
+  </div>,
+ 
   document.getElementById('root')
 );
 
